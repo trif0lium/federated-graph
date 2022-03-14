36b73542750a8d5682848975141e8a3ef367d8f0
trif0lium <gh@fleeingsunlight.dev>
2022-03-14 20:14:36 +0700

    feat(backend/commands): init

---
+++ edge/libs/backend/commands/src/lib/backend-commands.spec.ts
@@ -0,0 +1,7 @@
+import { backendCommands } from './backend-commands';
+
+describe('backendCommands', () => {
+  it('should work', () => {
+    expect(backendCommands()).toEqual('backend-commands');
+  });
+});
---
+++ edge/libs/backend/commands/src/lib/backend-commands.ts
@@ -0,0 +1,3 @@
+export function backendCommands(): string {
+  return 'backend-commands';
+}
