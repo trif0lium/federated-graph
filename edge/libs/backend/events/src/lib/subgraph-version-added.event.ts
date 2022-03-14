b49c30caac26b831cdabbd09913a4185ae29cdb3
trif0lium <gh@fleeingsunlight.dev>
2022-03-14 19:44:42 +0700

    fix(backend/events/subgraph-version-added): data -> subgraphVersionId

--- edge/libs/backend/events/src/lib/subgraph-version-added.event.ts
+++ edge/libs/backend/events/src/lib/subgraph-version-added.event.ts
@@ -1,3 +1,3 @@
-export class SubgraphVersionAdded {
-  constructor(public readonly subgraphId: string, public readonly data: string) { }
+export class SubgraphVersionAddedEvent {
+  constructor(public readonly subgraphId: string, public readonly subgraphVersionId: string) { }
 }
