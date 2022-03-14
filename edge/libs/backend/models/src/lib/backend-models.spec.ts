43939b4462dd8ea321d111b691afa0506a6e7699
trif0lium <gh@fleeingsunlight.dev>
2022-03-14 17:11:45 +0700

    feat(backend/models): init

---
+++ edge/libs/backend/models/src/lib/backend-models.spec.ts
@@ -0,0 +1,7 @@
+import { backendModels } from './backend-models';
+
+describe('backendModels', () => {
+  it('should work', () => {
+    expect(backendModels()).toEqual('backend-models');
+  });
+});
---
+++ edge/libs/backend/models/src/lib/backend-models.ts
@@ -0,0 +1,3 @@
+export function backendModels(): string {
+  return 'backend-models';
+}
