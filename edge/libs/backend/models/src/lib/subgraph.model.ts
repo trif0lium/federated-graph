19a0a2286a131297fc30219798a07b35e8aa48f4
trif0lium <gh@fleeingsunlight.dev>
2022-03-14 19:59:07 +0700

    feat(backend/models/subgraph): add routingUrl to constructor

--- edge/libs/backend/models/src/lib/subgraph.model.ts
+++ edge/libs/backend/models/src/lib/subgraph.model.ts
@@ -1,7 +1,10 @@
 import { AggregateRoot } from "@nestjs/cqrs";
 
 export class Subgraph extends AggregateRoot {
-  constructor(public readonly id: string, public readonly name: string) {
+  public readonly creationDate: Date
+  public readonly modificationDate: Date
+
+  constructor(public readonly id: string, public readonly name: string, public readonly routingUrl: string) {
     super()
   }
 }
