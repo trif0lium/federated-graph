0604eb89312a7c51506d21b4a449c2cb2115177e
trif0lium <gh@fleeingsunlight.dev>
2022-03-15 13:16:02 +0700

    feat(federated-graph): BackendFederatedGraphModule -> FederatedGraphModule

--- edge/libs/backend/federated-graph/src/lib/federated-graph.module.ts
+++ edge/libs/backend/federated-graph/src/lib/federated-graph.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CommandHandlers, EventHandlers } from './handlers';
 
 @Module({
+  imports: [CqrsModule],
   controllers: [],
-  providers: [],
+  providers: [
+    ...CommandHandlers,
+    ...EventHandlers
+  ],
   exports: [],
 })
-export class BackendFederatedGraphModule {}
+export class FederatedGraphModule { }
