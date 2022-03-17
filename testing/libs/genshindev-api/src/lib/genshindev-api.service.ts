7bd2e3fa7f37db268538de754e7965edf290e980
trif0lium <gh@fleeingsunlight.dev>
2022-03-17 14:53:16 +0700

    feat(genshindev-api): character()

--- testing/libs/genshindev-api/src/lib/genshindev-api.service.ts
+++ testing/libs/genshindev-api/src/lib/genshindev-api.service.ts
@@ -2,6 +2,7 @@ import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common'
 import { HttpService } from '@nestjs/axios'
 import { Cache } from 'cache-manager'
 import { lastValueFrom } from 'rxjs'
+import { Character } from './dto/character.dto'
 
 @Injectable()
 export class GenshindevApiService {
@@ -20,5 +21,17 @@ export class GenshindevApiService {
     const characters = await lastValueFrom(this.httpService.get<string[]>(api))
     await this.cacheManager.set(api, characters)
     return characters.data
+  }
+
+  async character(name: string): Promise<Character> {
+    const api = 'https://api.genshin.dev/characters/' + name
+    const cache = await this.cacheManager.get<Character>(api)
+    if (cache) {
+      return Promise.resolve<Character>(cache)
+    }
+
+    const character = await lastValueFrom(this.httpService.get<Character>(api))
+    await this.cacheManager.set(api, character)
+    return character.data
   }
 }
