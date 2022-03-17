ba2c3d1a23d2865dc51680dd4a3eb33eee573d84
trif0lium <gh@fleeingsunlight.dev>
2022-03-17 17:55:12 +0700

    feat(characters-dgs/app): use GraphQL Playground

--- testing/apps/characters-dgs/src/app/app.module.ts
+++ testing/apps/characters-dgs/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { CharacterModule } from '../character/character.module';
 import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo'
-
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -10,7 +10,8 @@ import { AppService } from './app.service';
   imports: [
     GraphQLModule.forRoot<ApolloFederationDriverConfig>({
       driver: ApolloFederationDriver,
-      autoSchemaFile: true
+      autoSchemaFile: true,
+      plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
     }),
     CharacterModule
   ],
