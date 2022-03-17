31dbf7b33e41ba549667a32f481027de1d38632e
trif0lium <gh@fleeingsunlight.dev>
2022-03-17 17:34:11 +0700

    feat(characters-dgs): add `characters` query

--- testing/apps/characters-dgs/src/character/character.resolver.ts
+++ testing/apps/characters-dgs/src/character/character.resolver.ts
@@ -1,4 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver, ResolveReference } from '@nestjs/graphql';
+import { Character } from '../entities/character.entity';
+import { CharacterService } from './character.service';
 
-@Resolver()
-export class CharacterResolver {}
+@Resolver(() => Character)
+export class CharacterResolver {
+  constructor(private readonly service: CharacterService) { }
+
+  @Query(() => [String])
+  async characters(): Promise<string[]> {
+    return this.service.characters()
+  }
+
+  @Query(() => Character)
+  async character(@Args('name') name: string): Promise<Character> {
+    return this.service.character(name)
+  }
+
+  @ResolveReference()
+  resolveReference(reference: { __typename: string; name: string }): Promise<Character> {
+    return this.service.character(reference.name)
+  }
+}
