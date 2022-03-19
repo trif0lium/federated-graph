c84f8efd20b37f280844d5492e3eebf93879e687
trif0lium <gh@fleeingsunlight.dev>
2022-03-19 16:15:11 +0700

    feat(genshin-api): GraphQLModule.forRoot()

--- testing/apps/genshin-api/src/app/app.module.ts
+++ testing/apps/genshin-api/src/app/app.module.ts
@@ -3,9 +3,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { IntrospectAndCompose } from '@apollo/gateway'
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+
+function supergraphSdl(): IntrospectAndCompose {
+  return new IntrospectAndCompose({
+    subgraphs: [
+      { name: 'characters-dgs', url: 'http://localhost:4001/graphql' },
+      { name: 'constellations-dgs', url: 'http://localhost:4002/graphql' },
+    ]
+  })
+}
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true
+      },
+      gateway: {
+        supergraphSdl: supergraphSdl()
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
