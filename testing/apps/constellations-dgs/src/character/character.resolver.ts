67f3c7a54bd5107c959be3e7fc3bcba48d5a4b67
trif0lium <gh@fleeingsunlight.dev>
2022-03-19 15:29:27 +0700

    feat(constellations-dgs/character): add `constellations` query

--- testing/apps/constellations-dgs/src/character/character.resolver.ts
+++ testing/apps/constellations-dgs/src/character/character.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { Character } from '../entities/character.entity';
+import { Constellation } from '../entities/constellation.entity';
+import { CharacterService } from './character.service';
 
-@Resolver()
-export class CharacterResolver {}
+@Resolver(() => Character)
+export class CharacterResolver {
+  constructor(private readonly service: CharacterService) { }
+
+  @ResolveField(() => [Constellation])
+  async constellations(@Parent() character: Character): Promise<Constellation[]> {
+    return this.service.constellations(character.name)
+  }
+}
