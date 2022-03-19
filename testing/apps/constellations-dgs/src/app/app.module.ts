395108ff4194f610bfe63bf5752343b35afebb4d
trif0lium <gh@fleeingsunlight.dev>
2022-03-19 15:34:11 +0700

    feat(constellations-dgs/app): GraphQLModule.forRoot()

--- testing/apps/constellations-dgs/src/app/app.module.ts
+++ testing/apps/constellations-dgs/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { CharacterModule } from '../character/character.module';
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})]
+    }),
+    CharacterModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
