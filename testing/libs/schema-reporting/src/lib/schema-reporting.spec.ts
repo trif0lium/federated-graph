ba84fae71c0790c793868b13523302c4ec60045f
trif0lium <gh@fleeingsunlight.dev>
2022-03-26 18:35:05 +0700

    chore(schema-reporting): delete .spec.ts

--- testing/libs/schema-reporting/src/lib/schema-reporting.spec.ts
+++
@@ -1,7 +0,0 @@
-import { schemaReporting } from './schema-reporting';
-
-describe('schemaReporting', () => {
-  it('should work', () => {
-    expect(schemaReporting()).toEqual('schema-reporting');
-  });
-});
