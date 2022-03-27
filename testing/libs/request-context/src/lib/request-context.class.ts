c04b9c962f1e7c0e04f928a788417f46b8e500f3
trif0lium <gh@fleeingsunlight.dev>
2022-03-27 18:22:13 +0700

    feat(request-context): add RequestContext abstract class

---
+++ testing/libs/request-context/src/lib/request-context.class.ts
@@ -0,0 +1,11 @@
+import { AsyncLocalStorage } from 'async_hooks'
+
+export abstract class RequestContext {
+  static asyncLocalStorage = new AsyncLocalStorage<RequestContext>();
+  static start = <T extends RequestContext>(constructor: new () => T): void => {
+    RequestContext.asyncLocalStorage.enterWith(new constructor())
+  }
+  static get<T extends RequestContext>(): T {
+    return RequestContext.asyncLocalStorage.getStore() as T;
+  }
+}
