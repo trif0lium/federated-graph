6b6a99a3830d32da212d337c143d4db2c2452139
trif0lium <gh@fleeingsunlight.dev>
2022-03-29 14:43:30 +0700

    feat(libs/logging): init LoggingService

---
+++ testing/libs/logging/src/lib/logging.service.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable, LoggerService, Scope } from '@nestjs/common'
+import { INQUIRER } from '@nestjs/core'
+import { context, trace } from '@opentelemetry/api'
+import pino from 'pino'
+import { RequestContext, REQUEST_CONTEXT } from '@testing/request-context'
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class LoggingService implements LoggerService {
+  private readonly logger = pino({
+    formatters: {
+      log(object) {
+        const span = trace.getSpan(context.active())
+        if (!span) {
+          return { ...object }
+        }
+        const { spanId, traceId } = span?.spanContext()
+        return { ...object, spanId, traceId }
+      }
+    }
+  })
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  private readonly source: string | object
+  private readonly requestContext?: RequestContext
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(@Inject(INQUIRER) source?: string | object, @Inject(REQUEST_CONTEXT) requestContext?: RequestContext) {
+    this.source = source
+    this.requestContext = requestContext
+  }
+}
+
